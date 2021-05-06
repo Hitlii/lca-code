0c713734e7728413c823985918117433036f5fd1
Cesar Ayala Vargas <62224256+Hitlii@users.noreply.github.com>
2021-05-05 19:15:34 -0700

    COORS and AUTH
    Se arregla el proeblema de COORS para el end-point 'post-images'
    Tambien se resuleve el problema de AUTH, ahora este sera un middleware FUERA  de /graphql esto no deberia de afectar a lo implementado en frontend

--- backend/src/graphql/resolvers/middleware/index.js
+++ backend/src/graphql/resolvers/middleware/index.js
@@ -1,9 +1,12 @@
 const { skip } = require('graphql-resolvers')
 
 // Middleware, checks if user is authenticated
-module.exports.isAuthenticated = (parent, args, { email }) => {
-  if (!email) {
-    throw new Error('Access Denied! Please login to continue')
+module.exports.isAuthenticated = (parent, args, { isAuth }) => {
+  if (!isAuth) {
+    const error = new Error('Acceso denegado!');
+    error.code = 401;
+    error.solution = 'Inicie sesion para continuar c:'
+    throw error;
   }
   return skip
 }
