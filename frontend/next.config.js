f3d6d5452dfc81a7c2ca2e693d74cda32f6016f9
LangostaFeliz <fhaosheng@gmail.com>
2021-08-01 13:42:01 -0700

    fixed material ui bug in production

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,8 @@
-const isProd = process.env.NODE_ENV === 'production'
+//const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   // CDN Roboto Font Declaration
-  assetPrefix: isProd ? 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap' : '',
+//assetPrefix: isProd ? 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap' : '',
   env: {
     googleMapsAPIKey: 'AIzaSyDVEF5nE9KpC2qQ6mKLly5eYkiXwMnoDqs'
   },
