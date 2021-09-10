e3affdb8e8f5b50d7c12bf8e751a7b12ec2e51e4
Cesar Ayala Vargas <62224256+Hitlii@users.noreply.github.com>
2021-09-10 16:28:11 -0700

    Production changes
    Eliminamos temporalmente lo que sea del Admin.

--- frontend/pages/admin1/propiedades/vender-propiedad.js
+++
@@ -1,9 +0,0 @@
-// Formulario para vender una propiedad (Tickets)
-
-export default function SellPropertyPage(){
-    return(
-        <div>
-            <h1>Formulario de venta de propiedad</h1>
-        </div>
-    )
-}
